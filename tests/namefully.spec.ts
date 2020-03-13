79acad6a7cbabd2634411901a316a9e20ccd67e4
Ralph Florent <ralflornt@gmail.com>
2020-03-13 05:34:51 +0100

    test: cover some namefully methods test cases
    some tests are failin so far... see logs.

--- tests/namefully.spec.ts
+++ tests/namefully.spec.ts
@@ -23,6 +23,11 @@ describe('Namefully', () => {
     ].join(' ')
     const name = new Namefully(fullname)
 
+    beforeAll(() => {
+        jest.spyOn(console, 'warn')
+        jest.spyOn(name, 'compress')
+    })
+
     test('should create an instance using literal string', () => {
         expect(name).toBeInstanceOf(Namefully)
     })
@@ -45,6 +50,71 @@ describe('Namefully', () => {
 
     test('should return expected suffix', () => {
         expect(name.getSuffix()).toEqual(suffix)
+    })
+
+    test('should return expected initials', () => {
+        expect(name.getInitials()).toEqual(['J', 'S'])
+    })
+
+    test('should shorten name to first and last names', () => {
+        const expected = `${firstname} ${lastname}`
+        expect(name.shorten()).toEqual(expected)
+    })
+
+    test('should describe statistically a name', () => {
+        const summary = name.describe()
+        expect(summary).toContain('count')
+        expect(summary).toContain('frequency')
+        expect(summary).toContain('top')
+        expect(summary).toContain('unique')
+    })
+
+    test('should try to compress middlename by default', () => {
+        const compressed = name.compress()
+        expect(compressed).toContain(firstname)
+        expect(compressed).toContain(lastname)
+    })
+
+    test('should limit name to 10 chars', () => {
+        const compressed = name.compress(10)
+        expect(compressed).toBe(`${firstname} J. ${lastname}`)
+        expect(console.warn).toBeCalled()
+        expect(console.warn).toBeCalledTimes(1)
+    })
+
+    test('should limit name to 10 chars by compressing the firstname', () => {
+        const compressed = name.compress(10, 'firstname')
+        expect(compressed).toBe(`J. ${middlename} ${lastname}`)
+        expect(console.warn).toBeCalled()
+        expect(console.warn).toBeCalledTimes(1)
+    })
+
+    test('should limit name to 10 chars by compressing the lastname', () => {
+        const compressed = name.compress(10, 'lastname')
+        expect(compressed).toBe(`${firstname} ${middlename} S.`)
+        expect(console.warn).toBeCalled()
+        expect(console.warn).toBeCalledTimes(1)
+    })
+
+    test('should limit name to 10 chars by compressing the firstmid', () => {
+        const compressed = name.compress(10, 'firstmid')
+        expect(compressed).toBe(`J. J. ${lastname}`)
+        expect(console.warn).toBeCalled()
+        expect(console.warn).toBeCalledTimes(1)
+    })
+
+    test('should limit name to 10 chars by compressing the midlast', () => {
+        const compressed = name.compress(10, 'midlast')
+        expect(compressed).toBe(`${firstname} J. S.`)
+        expect(console.warn).toBeCalled()
+        expect(console.warn).toBeCalledTimes(1)
+    })
+
+    test('should output possible usernames', () => {
+        const usernames = name.username()
+        expect(usernames).toEqual(expect.arrayContaining([
+            'j.smith', 'jsmith', 'johnsmith', 'josmith'
+        ]))
     })
 
     // TODO: to be continued...
